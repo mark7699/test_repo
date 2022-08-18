00df967af7fa8198451d47d51a695ed49330afd9
mark Stokes <markstokes@marks-mbp.home>
2022-08-18 15:18:50 +0100

    add cancel button

--- test3B.py
+++ test3B.py
@@ -24,8 +24,9 @@ def open_popup():
     pop.attributes('-fullscreen', True)
     B = Button(pop, text="CANCEL", command=pop.destroy)
     B.place(x=200, y=200)
-    B = Button(pop, text="POWER\n OFF", bg="red", height=3, width=6,command="shutdown -P")
+    B = Button(pop, text="POWER\n OFF", bg="red", height=3, width=6, command="poweroff")
     B.place(x=350, y=400)
+    subprocess.call(command, shell=True)
     count=0
     errorlabel = Label(pop, text="You have 3 attempts.", font=('Mistral 10 bold'))
     errorlabel.place(x=300, y=45)
