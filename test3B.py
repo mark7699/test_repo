8dc32ddee30eab845694328dbf857b77e914edcb
mark Stokes <markstokes@marks-mbp.home>
2022-08-26 22:00:58 +0100

    add cancel button

--- test3B.py
+++ test3B.py
@@ -102,7 +102,7 @@ def iPhone():
     msg=messagebox.showinfo("iPhone Forensics Back Up", "Backup Complete of "+(p)+"& stored at /home/kali/CaseData")
 
 def Unlock():
-    command="cd Desktop; gnome-terminal --window --full-screen"
+    command="cd Desktop; gnome-terminal --window --full-screen sudo ./checkra1n"
     subprocess.call(command, shell=True)
     msg = messagebox.showinfo(" Unlock iPhone", "Connect iPhone with USB and place in DFU mode\n Note add DFU Instructions here")
 
